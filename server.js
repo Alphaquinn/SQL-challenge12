4cac13eb103822caec570f1abe90e1577e26451e
AlphaQuinn <snowboarderquinn@gmail.com>
2023-02-22 12:14:51 -0500

    finished server in prompts for inputs

--- server.js
+++ server.js
@@ -62,3 +62,122 @@ function addDepartment(){
         });
     });
 }
+
+//adds new role  
+
+function addrole(){
+    db.query("select the id and name of the department",(err, department)=>{
+        prompt([
+            {
+                name: "title",
+                type:"input",
+                message: "what is the title of the rol that you would like to add", 
+            },
+            {
+                name:"salary",
+                type:"input",
+                message: "what is the salary of this role?",
+
+
+            },
+            {
+                name: "department_id",
+                type: "list",
+                choices: department.map(x=>({name: x.name, value: x.id})),
+                message:"what would the department id  be for this added role?",
+            },
+        ]).then(function(answer){
+            db.query("INSERT INTO role SET?",[answer], function(err){
+                if (err) throw err;
+                console.log("success");
+                employerOption();
+            });
+        });
+    })
+}
+
+
+
+// adds a neew employee
+
+function addEmployee(){
+    db.query("select the id, title from the role", (err, manager)=>{
+        db.query("select id, last_name from employee", (err, manager)=>{
+            prompt([
+                {
+                    name: "first_name",
+                    type: "input",
+                    message: "what is the first name of the employee that you wuld like to add",
+
+                },
+                {
+                    name: "last_name",
+                    type:"input",
+                    message: "what is the last name of the employee that you would like to add",
+                },
+                {
+                    name:"role_id",
+                    type: "list",
+                    choices: role.map(x=>({name:x.title,value:x.id})),
+                    message: "what is the id if the role for this employee?",
+
+
+                },
+                {
+                    name: "manager_id",
+                    type:"list",
+                    choices: manager.map(x=>({name: x.last_name, value: x.id})),
+                    message: "what is the manager id for this new employee?",
+                },
+            ]).then(function(answer){
+                db.query("INSERT INTO employee SET?",[answer], function(err){
+                    if (err) throw err;
+                    console.log("success");
+                    employerOption();
+                });
+            });
+        })
+    })
+}
+
+function viewAllroles(){
+   
+    db.query( "SELECT role.title, role.salary, department.name, FROM fole LEFT JOIN department on role.department_id=department.id",
+        function(err, res){
+            if(err)throw err;
+            console.table(res);
+            employerOption
+        }
+    );
+}
+
+
+function viewAllDepartments(){
+    db.query("SELECT * FROM department", function(err, res){
+        if (err) throw err;
+        console.table(res);
+        employerOption
+    });
+}
+
+function viewAllEmployees(){
+    db.query( "SELECT employee.first_name, employee.last_name, role.title, role.salary, department.name, manager.first_name AS 'manager_firstname',, manager.last_name AS 'manager_lastname' FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN employee manager ON employee.manager_id = manager.id;",
+        function(err,res){
+            if (err)throw err;
+            console.table(res);
+            employerOption
+        }
+    );
+}
+
+
+
+
+
+employerOption();
+
+db.connect((err)=>{
+    if (err) throw err;
+    console.log("successfully connected");
+
+});
